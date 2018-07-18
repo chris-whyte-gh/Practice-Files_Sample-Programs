cdebdbff80478853ccdb0e1577d111a88c88edae
Christopher Whyte <cwhyte@rsglab.com>
2018-07-17 20:13:40 -0400

    Adding rectangle practice file

---
+++ practice.rb
@@ -0,0 +1,13 @@
+#area of a rectangle is length x width, store the value in a variable
+def area(length, width)
+    return length * width
+end
+
+#perimeter of a rectangle is length + width x 2, stored in a variable
+def perimeter(length, width)
+    return (2 * (length + width))
+end
+
+puts area(3, 6)
+puts area(4, 4)
+puts perimeter(1, 5)
